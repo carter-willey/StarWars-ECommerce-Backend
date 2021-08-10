aec1e9aa0405c691d6919ddd8d722a8e187aa123
CyrusWarner <cyruswarner27@gmail.com>
2021-08-10 17:43:52 -0500

    Added Product Controller with Get and post functionality.

---
+++ eCommerceStarterCode/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<ProductController>
+        //GETS ALL PRODUCTS 
+        [HttpGet]
+        public IActionResult Get()
+        {
+            
+            var products = _context.Products;
+            return Ok(products);
+        }
+
+        //GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var product = _context.Products.Where(product => product.ProductId == id);
+            return Ok(product); 
+        }
+
+        //// POST api/<ProductController>
+        [HttpPost]
+        public IActionResult Post([FromBody]Product value)
+        {
+            _context.Products.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+
+        //// PUT api/<ProductController>/5
+        //[HttpPut("{id}")]
+        //public IActionResult Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<ProductController>/5
+        //[HttpDelete("{id}")]
+        //public IActionResult Delete(int id)
+        //{
+        //}
+    }
+}
