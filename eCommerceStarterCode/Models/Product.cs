8e1580c38e9a4ddc3105e08a84b31328880fa451
CyrusWarner <cyruswarner27@gmail.com>
2021-08-10 15:57:38 -0500

    Added Properties for the product model.

--- eCommerceStarterCode/Models/Product.cs
+++ eCommerceStarterCode/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,19 @@ namespace eCommerceStarterCode.Models
 {
     public class Product
     {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; } 
+        public string Description { get; set; }
+        public int AverageRating {  get; set; }
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+        [ForeignKey("Review")]
+        public int ReviewId { get; set; }
+        public Review Review { get; set; }
     }
 }
