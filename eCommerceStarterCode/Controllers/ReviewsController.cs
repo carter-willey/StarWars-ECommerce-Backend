7c9f4d02635f2bac7b4ee7d770ac39c489c4d2c1
Andy Wade <carrollandywadejr@gmail.com>
2021-08-11 11:56:04 -0500

    added controller for reviews model

---
+++ eCommerceStarterCode/Controllers/ReviewsController.cs
@@ -0,0 +1,69 @@
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        public ReviewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<ReviewsController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var reviews = _context.Reviews;
+            return Ok(reviews);
+        }
+
+        // GET api/<ReviewsController>/5
+        //[HttpGet("{id}")]
+        //public IActionResult Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/<ReviewsController>
+        [HttpPost, Authorize]
+        public IActionResult Post([FromBody] Review value)
+        {
+            _context.Reviews.Add(value);
+            _context.SaveChanges();
+            return Ok(value);
+        }
+
+        // PUT api/<ReviewsController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Review value)
+        {
+            var review = _context.Reviews.FirstOrDefault(review => review.ReviewId == id);
+            review.Description = value.Description;
+            review.Rating = value.Rating;
+            review.UserId = value.UserId;
+            _context.SaveChanges();
+            return Ok(review);
+        }
+
+        // DELETE api/<ReviewsController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var review = _context.Reviews.FirstOrDefault(review => review.ReviewId == id);
+            _context.Remove(review);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
