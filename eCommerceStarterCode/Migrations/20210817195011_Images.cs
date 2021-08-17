427afda9224bb5ff3fda9964c5fc1bada28b81a3
carter-willey <cjwilley23@gmail.com>
2021-08-17 14:50:53 -0500

    added migrations for product images

---
+++ eCommerceStarterCode/Migrations/20210817195011_Images.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eCommerceStarterCode.Migrations
+{
+    public partial class Images : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "291b4388-34b6-4ba9-aa9a-61aff9a90127");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "57092b51-d9cd-4f23-b5c4-f35282579dc8");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Products");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "291b4388-34b6-4ba9-aa9a-61aff9a90127", "96273819-46b8-474c-b736-6e76c7a43dbc", "User", "USER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "57092b51-d9cd-4f23-b5c4-f35282579dc8", "81cca9c9-fb51-4387-942c-17a8d75f197b", "Admin", "ADMIN" });
+        }
+    }
+}
